64000cf25583c3930bc2fc76fb89aeae62d1d3d8
Darth Livesey <szajcev504@gmail.com>
2025-03-10 05:39:31 +0300

    Второй шаг

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,9 +5,9 @@ def main():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
     )
+    parser.add_argument("-f", "--format", help = "set format of output")
     parser.add_argument("first_file")
     parser.add_argument("second_file")
     args = parser.parse_args()
-    print(args.first_file, args.second_file)
 
 main()
