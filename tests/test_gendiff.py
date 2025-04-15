971754cea5247ff9975633b6107e122ccf255582
Darth Livesey <szajcev504@gmail.com>
2025-04-15 05:05:00 +0300

    Пятый шаг. Исправление ошибок линтера

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,7 +2,8 @@ from gendiff.scripts.generate_diff import generate_difference
 
 
 def test_generate_difference():
-    assert str(generate_difference("tests/file1.json", "tests/file2.json")) == """{
+    assert str(generate_difference("tests/file1.json",
+                                    "tests/file2.json")) == """{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
