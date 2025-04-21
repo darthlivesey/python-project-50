9af426c455c130081875ae4c797e5408476164ca
Darth Livesey <szajcev504@gmail.com>
2025-04-21 08:41:24 +0300

    Восьмой шаг. Исправление ошибки линтера

--- gendiff/scripts/generate_stylish_view.py
+++ gendiff/scripts/generate_stylish_view.py
@@ -2,7 +2,8 @@ def generate_stylish_view(input, deep=0):
     result_string = "{\n"
     for element in input:
         if type(element[2]) is str:
-            result_string += deep * "    " + " ".join([i.strip("'") for i in element]) + "\n"
+            result_string += deep * "    " + " ".join(
+                [i.strip("'") for i in element]) + "\n"
         else:
             result_string += deep * "    " + (
                 f"{element[0]} {element[1]} " 
